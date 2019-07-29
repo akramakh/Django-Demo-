37bfee4978433d6ef574842672f3d3bf5a528c31
akramakh <akram.isocial@gmail.com>
2019-07-29 17:26:28 +0300

    initial commit

--- demo/settings.py
+++ demo/settings.py
@@ -149,7 +149,7 @@ MEDIA_URL = '/media/'
 
 
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
